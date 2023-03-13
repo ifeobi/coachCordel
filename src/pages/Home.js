47bf0300bff93a0003356ab488c14d934f648a1d
abc <abc@example.com>
2023-03-13 14:13:50 +0100

    whatsapp button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,10 @@ function Home() {
   return (
     <div className="home">
       <nav className="hea">
-        <h1 style={{cursor: 'pointer'}}>Coach Cordel</h1>
-       <Link to='/tips' style={{textDecoration: "none", color: "white"}}><h1 style={{cursor: 'pointer'}}>Exam Tips</h1></Link>
+        <h1 style={{ cursor: "pointer" }}>Coach Cordel</h1>
+        <Link to="/tips" style={{ textDecoration: "none", color: "white" }}>
+          <h1 style={{ cursor: "pointer" }}>Exam Tips</h1>
+        </Link>
       </nav>
       <main className="body">
         <div className="content">
@@ -18,15 +20,27 @@ function Home() {
               <b>IELTS & TOEFL candidates in Abuja, FCT </b>get the scores they
               deserve to change their lives!
             </p>
-            <br/>
-            <a href="tel:+2348065068366" className="button">Book an Appointment</a>
+            <br />
+            <a href="tel:+2348065068366" className="button">
+              Book an Appointment
+            </a>
           </div>
         </div>
         <div className="image"></div>
       </main>
       <div className="footer">
-        <p> Phone: +234 806 506 8366, Suite A23 Garima Plaza Arab Road, Kubwa, FCT, Nigeria</p>
+        <p>
+          {" "}
+          Phone: +234 806 506 8366, Suite A23 Garima Plaza Arab Road, Kubwa,
+          FCT, Nigeria
+        </p>
       </div>
+      <a
+        href="https://wa.me/+2348065068366?text=Hello%2C%20I%20have%20a%20question%20about%20your%20services."
+        className="float"        
+      >
+        <i className="fa fa-whatsapp my-float"></i>
+      </a>
     </div>
   );
 }
